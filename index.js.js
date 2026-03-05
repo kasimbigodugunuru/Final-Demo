d64373e2dff8ff4aa69e6ba370180445c279b76d
kasimbigodugunuru <kasimbigodugunuru6@gmail.com>
2026-03-05 15:17:53 +0530

    Add files via upload

---
+++ index.js.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Final Demo Deployment Successful!");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
